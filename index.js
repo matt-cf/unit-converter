876a623c364cac0b1f4cdcdcf6389b0934ba8970
Matthew <cheng.f.lou@gmail.com>
2023-06-12 23:07:31 +0800

    added conversion to display on webpage

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 
 const convertBtn = document.getElementById("convert-btn")
+let lengthText = document.getElementById("length");
+let volumeText = document.getElementById("volume");
+let massText = document.getElementById("mass");
 
 convertBtn.addEventListener("click", function() {
     let input = Number(document.getElementById("userInput").value);
-    
     length(input);
     volume(input);
-    weight(input);
+    mass(input);
 })
 
 function length(num) {
     toFeet = (num * 3.281).toFixed(3);
     toMeter = (num / 3.281).toFixed(3);
-    console.log(toFeet, toMeter);
+    let sentence = `${toMeter} meters | ${toFeet} feet`;
+    lengthText.textContent = sentence
 }
 
 function volume(num) {
     toGallon = (num * 0.264).toFixed(3);
     toLiter = (num / 0.264).toFixed(3);
-    console.log(toGallon, toLiter)
+    let sentence = `${toGallon} gallons | ${toLiter} liters`;
+    volumeText.textContent = sentence
 }
 
-function weight(num) {
+function mass(num) {
     toKilogram = (num * 2.204).toFixed(3);
-    toPound = (num / 2.204).toFixed(3)
-    console.log(toKilogram, toPound)
+    toPound = (num / 2.204).toFixed(3);
+    let sentence = `${toKilogram} kilograms | ${toPound} pounds`;
+    massText.textContent = sentence
 }
